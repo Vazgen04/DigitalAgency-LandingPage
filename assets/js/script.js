9481ccf8655a6ffc7245d8d75d14fc9316c5c256
Vazgen <99919037+Vazgen04@users.noreply.github.com>
2022-08-20 16:07:19 +0400

    Mobile menu has been created

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const mobile = document.querySelector(".mobile");
+const body = document.querySelector(".body");
+
+function Menu() {
+    mobile.classList.toggle("active");
+    body.classList.toggle("overflow");
+}
