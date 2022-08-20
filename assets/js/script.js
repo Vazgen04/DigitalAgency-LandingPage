194eed86212a5e69b1eadad498c75983df119515
Vazgen <99919037+Vazgen04@users.noreply.github.com>
2022-08-20 16:15:39 +0400

    Project done (after adding animations)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const mobile = document.querySelector(".mobile");
 const body = document.querySelector(".body");
 
+AOS.init();
+
 function Menu() {
     mobile.classList.toggle("active");
     body.classList.toggle("overflow");
